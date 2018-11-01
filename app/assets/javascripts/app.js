69ea979609c437c2862b8b9640474462f7cc176a
therrance <lozuvatka@gmail.com>
2018-11-01 12:13:06 +0000

    move views to modules

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,57 +1,4 @@
-angular.module('flapperNews', ['ui.router'])
-    .controller('MainCtrl', [
-        '$scope', 'posts',
-        function($scope, posts) {
-            $scope.test = 'Hello world!';
-
-            $scope.posts = posts.posts;
-
-            $scope.addPost = function() {
-                if (!$scope.title || $scope.title === '') {
-                    return;
-                }
-                $scope.posts.push({
-                    title: $scope.title,
-                    link: $scope.link,
-                    upvotes: 0,
-                    comments: [
-                        { author: 'Joe', body: 'Cool post!', upvotes: 0 },
-                        { author: 'Bob', body: 'Great idea!', upvotes: 0 },
-                    ]
-                });
-                $scope.title = '';
-                $scope.link = '';
-            };
-
-            $scope.incrementUpvotes = function(post) {
-                post.upvotes += 1;
-            };
-        }
-    ])
-    .controller('PostsCtrl', [
-        '$scope',
-        '$stateParams',
-        'posts',
-        function($scope, $stateParams, posts) {
-            $scope.post = posts.posts[$stateParams.id];
-
-            $scope.addComment = function() {
-                if ($scope.body === '') { return; }
-                $scope.post.comments.push({
-                    body: $scope.body,
-                    author: 'user',
-                    upvotes: 0
-                });
-                $scope.body = '';
-            };
-        }
-    ])
-    .factory('posts', [function() {
-        var o = {
-            posts: []
-        };
-        return o;
-    }])
+angular.module('flapperNews', ['ui.router', 'templates'])
     .config([
         '$stateProvider',
         '$urlRouterProvider',
@@ -60,12 +7,12 @@ angular.module('flapperNews', ['ui.router'])
             $stateProvider
                 .state('home', {
                     url: '/home',
-                    templateUrl: '/home.html',
+                    templateUrl: 'home/_home.html',
                     controller: 'MainCtrl'
                 })
                 .state('posts', {
                     url: '/posts/{id}',
-                    templateUrl: '/posts.html',
+                    templateUrl: 'posts/_posts.html',
                     controller: 'PostsCtrl'
                 });
 
